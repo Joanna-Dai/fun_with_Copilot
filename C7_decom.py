d2dbc7ca6f2064d45ccc0923141d85dd33feeec3
Joanna-Dai <joan.di1223@gmail.com>
2025-01-01 11:06:52 +0800

    20250101

--- C7_decom.py
+++ C7_decom.py
@@ -141,3 +141,125 @@ def split_string(text, separators):
         words.append(word)
     
     return words
+
+
+
+def get_sentence(text):
+    """
+    text is a string of text.
+
+    return a list o the setences from text. 
+    sentences are seperated by '.', '!', or '?'.
+
+    >>> get_sentence('A pearl! Pearl! Lustrous pearl! Rare. What a nice find.')
+    ['A pearl', ' Pearl', ' Lustrous pearl', ' Rare', ' What a nice find']
+
+    """
+    return split_string(text, '.!?')
+
+
+
+def average_sentence_length(text):
+    """
+    text is a string of text.
+
+    return the average number of words per sentence in text.
+    do not count empty wrds as words.
+
+    >>> average_sentence_length('A pearl! Pearl! Lustrous pearl! Rare. What a nice find.')
+    2.0
+
+    """
+    sentences = get_sentence(text)
+    total = 0
+    count = 0
+    for sentence in sentences:
+        words = sentence.split()
+        for word in words:
+            word = clean_word(word)
+            if word != '':
+                total += 1
+        count += 1
+    
+    return total / count
+
+
+
+def get_phrases(sentence):
+    """
+    sentence is a sentence string.
+
+    return a list of the phrases from sentence.
+    phrases are seperated by ',' or ';' or ':'.
+
+    >>> get_phrases('A pearl, Pearl; Lustrous pearl: Rare.')
+    ['A pearl', ' Pearl', ' Lustrous pearl', ' Rare']
+
+    """
+    return split_string(sentence, ',;:')
+
+
+
+def average_sentence_complexity(text):
+    """
+    text is a string of text.
+
+    return the average number of phrases per sentence in text.
+
+    >>> average_sentence_complexity('A pearl! Pearl! Lustrous pearl! Rare.\
+        What a nice find.')
+    1.0
+
+    >>> average_sentence_complexity('A pearl! Pearl! Lustrous pearl! Rare, \
+        what a nice find.')
+    1.25
+
+    """
+    sentences = get_sentence(text)
+    total = 0
+    count = 0
+    for sentence in sentences:
+        phrases = get_phrases(sentence)
+        for phrase in phrases:
+            total += 1
+        count += 1
+    return total / count
+
+
+
+def make_signature(text):
+    """
+    the signature for text is a list of five elements.
+    average word length, different words to total words, \
+    words used exactly once to total words, average sentence length, \
+    average sentence complexity.
+
+    return the signature for text.
+
+    >>> make_signature('A pearl! Pearl! Lustrous pearl! Rare, What a nice find.')
+    [4.1, 0.7, 0.5, 2.5, 1.25]
+    
+    """
+    return [average_word_length(text), different_to_total(text), \
+            exactly_once_to_total(text), average_sentence_length(text), \
+            average_sentence_complexity(text)]
+
+
+
+def get_all_signature(known_dir):
+    """
+    known_dir is the name of a directory of books.
+    for each file in directory known_dir, determine its signature.
+
+    return a dictionary where each key is
+    the name of a file, and the value is its signature.
+
+    """
+    import os
+    signatures = {}
+    for file in os.listdir(known_dir):
+        with open(known_dir + '/' + file, 'r') as f:
+            text = f.read()
+            signatures[file] = make_signature(text)
+    
+    return signatures
