3147e432ebef3c6f6ecd3e35e7893f872ab49d5d
Joanna-Dai <joan.di1223@gmail.com>
2025-01-11 12:05:24 +0800

    20250111

---
+++ C9_autom.py
@@ -0,0 +1,10 @@
+import PyPDF2
+
+def merge_pdfs(pdf_list, output_path):
+    pdf_merger = PyPDF2.PdfMerger()
+
+    for pdf in pdf_list:
+        pdf_merger.append(pdf)
+
+    with open(output_path, 'wb') as output_file:
+        pdf_merger.write(output_file)
